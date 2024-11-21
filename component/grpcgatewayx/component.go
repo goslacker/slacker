843353e03a50e58d8c97728073ab6985a9936afa
zed <zedsmail@163.com>
2024-11-21 23:49:00 +0800

    feat: 增加请求响应日志中间件

--- component/grpcgatewayx/component.go
+++ component/grpcgatewayx/component.go
@@ -1,12 +1,16 @@
 package grpcgatewayx
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
-	"google.golang.org/protobuf/types/known/emptypb"
+	"io"
 	"log/slog"
 	"net/http"
 	"strings"
+
+	"google.golang.org/protobuf/types/known/emptypb"
 
 	"github.com/goslacker/slacker/core/app"
 
@@ -127,6 +131,7 @@ func (c *Component) Start() {
 	if c.errorHandler != nil {
 		options = append(options, runtime.WithErrorHandler(c.errorHandler))
 	}
+	options = append(options, runtime.WithMiddlewares(LogReqAndRespMiddleware))
 	mux := runtime.NewServeMux(options...)
 	for _, register := range c.registers {
 		err := register(ctx, mux, conn)
@@ -161,5 +166,74 @@ func (c *Component) Stop() {
 	c.gwServer.Shutdown(ctx)
 	if c.cancel != nil {
 		c.cancel()
+	}
+}
+
+type ResponseLogger struct {
+	http.ResponseWriter
+	StatusCode int
+	Body       []byte
+}
+
+func (r *ResponseLogger) WriteHeader(statusCode int) {
+	r.StatusCode = statusCode
+	r.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (r *ResponseLogger) Write(body []byte) (int, error) {
+	r.Body = body
+	return r.ResponseWriter.Write(body)
+}
+
+// LogReqAndRespMiddleware 是一个中间件，用于打印请求和响应的日志
+func LogReqAndRespMiddleware(next runtime.HandlerFunc) runtime.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request, pathParams map[string]string) {
+		reqBody, err := io.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+		r.Body = io.NopCloser(bytes.NewReader(reqBody))
+
+		w = &ResponseLogger{ResponseWriter: w}
+
+		// 调用下一个处理函数
+		next(w, r, pathParams)
+
+		rMap := map[string]any{
+			"method": r.Method,
+			"url":    r.URL.String(),
+			"header": r.Header,
+		}
+		{
+			var b map[string]any
+			err = json.Unmarshal(reqBody, &b)
+			if err != nil {
+				rMap["body"] = string(reqBody)
+			} else {
+				rMap["body"] = b
+			}
+		}
+
+		respMap := map[string]any{
+			"status": w.(*ResponseLogger).StatusCode,
+			"header": w.Header(),
+		}
+		{
+			var b map[string]any
+			err = json.Unmarshal(w.(*ResponseLogger).Body, &b)
+			if err != nil {
+				respMap["body"] = string(w.(*ResponseLogger).Body)
+			} else {
+				respMap["body"] = b
+			}
+		}
+
+		slog.Debug(
+			"request log",
+			"req",
+			rMap,
+			"resp",
+			respMap,
+		)
 	}
 }
