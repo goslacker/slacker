190279b100bfc25f02adbde499593758a6f882c4
zed <zedsmail@163.com>
2025-06-08 19:22:11 +0800

    feat: etcd resolver 断线重连

--- component/grpcx/resolver/etcd.go
+++ component/grpcx/resolver/etcd.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/spf13/viper"
 	"go.etcd.io/etcd/api/v3/mvccpb"
 	"go.etcd.io/etcd/api/v3/v3rpc/rpctypes"
 	clientv3 "go.etcd.io/etcd/client/v3"
@@ -15,7 +16,6 @@ import (
 
 	"github.com/goslacker/slacker/core/serviceregistry/registry"
 	"github.com/goslacker/slacker/core/trace"
-	"github.com/spf13/viper"
 	"google.golang.org/grpc/resolver"
 )
 
@@ -28,41 +28,53 @@ type Config struct {
 }
 
 func NewEtcdResolver(target resolver.Target, cc resolver.ClientConn) *etcdResolver {
-	r := &etcdResolver{
-		target: target,
-		cc:     cc,
-	}
-
 	var conf Config
 	err := viper.Sub("grpcx").Unmarshal(&conf)
 	if err != nil {
 		slog.Error("new registry failed", "error", err)
 		return nil
 	}
-	r.c, err = clientv3.New(clientv3.Config{
-		Endpoints: conf.Registry.Endpoints,
-	})
+
+	r := &etcdResolver{
+		target:    target,
+		cc:        cc,
+		endpoints: conf.Registry.Endpoints,
+	}
+
+	r.c, err = r.newClient()
 	if err != nil {
 		slog.Error("new etcd client failed", "error", err)
 		return nil
 	}
 
-	shouldWatch, addrs, err := r.resolve(target.Endpoint())
+	shouldWatch, addrs, rev, err := r.resolve(target.Endpoint())
 	if err != nil {
 		slog.Error("service resolve failed", "service", target.Endpoint(), "error", err)
 	}
 
 	if shouldWatch {
-		go r.watch(target.Endpoint(), addrs)
+		go r.watch(target.Endpoint(), addrs, rev)
 	}
 
 	return r
 }
 
 type etcdResolver struct {
-	target resolver.Target
-	cc     resolver.ClientConn
-	c      *clientv3.Client
+	target    resolver.Target
+	cc        resolver.ClientConn
+	c         *clientv3.Client
+	endpoints []string
+}
+
+func (r *etcdResolver) newClient() (c *clientv3.Client, err error) {
+	c, err = clientv3.New(clientv3.Config{
+		Endpoints: r.endpoints,
+	})
+	if err != nil {
+		err = fmt.Errorf("new etcd client failed: %w(endpoints=%v)", err, r.endpoints)
+		return
+	}
+	return
 }
 
 func (r *etcdResolver) ResolveNow(o resolver.ResolveNowOptions) {}
@@ -71,7 +83,7 @@ func (r *etcdResolver) Close() {
 	r.c.Close()
 }
 
-func (r *etcdResolver) resolve(target string) (shouldWatch bool, addrs map[string]resolver.Address, err error) {
+func (r *etcdResolver) resolve(target string) (shouldWatch bool, addrs map[string]resolver.Address, rev int64, err error) {
 	ipReg := regexp.MustCompile(`^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}`)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
@@ -92,6 +104,7 @@ func (r *etcdResolver) resolve(target string) (shouldWatch bool, addrs map[strin
 			return
 		}
 		shouldWatch = true
+		rev = resp.Header.Revision
 	} else {
 		err = r.cc.UpdateState(resolver.State{Addresses: []resolver.Address{
 			{Addr: target},
@@ -104,24 +117,21 @@ func (r *etcdResolver) resolve(target string) (shouldWatch bool, addrs map[strin
 	return
 }
 
-func (r *etcdResolver) watch(prefix string, addrs map[string]resolver.Address) {
+func (r *etcdResolver) watch(prefix string, addrs map[string]resolver.Address, rev int64) {
 	for {
 		slog.Debug("watch service", "prefix", prefix, "addrs", addrs)
 		ctx, cancel := context.WithCancel(context.Background())
 		opts := []clientv3.OpOption{
 			clientv3.WithPrefix(),
 		}
+		if rev != 0 {
+			opts = append(opts, clientv3.WithRev(rev+1))
+		}
 		rch := r.c.Watch(ctx, prefix, opts...)
 		for n := range rch {
 			if n.Err() != nil {
-				cancel()
 				if errors.Is(n.Err(), rpctypes.ErrCompacted) {
 					slog.Debug("watch compacted, reload", "prefix", prefix)
-					var err error
-					_, addrs, err = r.resolve(prefix)
-					if err != nil {
-						slog.Error("service resolve failed", "service", prefix, "error", err)
-					}
 				} else {
 					slog.Error("watch service failed", "prefix", prefix, "error", n.Err())
 				}
@@ -155,8 +165,21 @@ func (r *etcdResolver) watch(prefix string, addrs map[string]resolver.Address) {
 			}
 		}
 		cancel()
+		r.c.Close()
 		s := rand.IntN(10) + 1
 		time.Sleep(time.Second * time.Duration(s))
+		slog.Info("re new etcd client")
+		var err error
+		r.c, err = r.newClient()
+		if err != nil {
+			slog.Debug("re new etcd client failed", "error", err)
+			continue
+		}
+
+		_, addrs, rev, err = r.resolve(prefix)
+		if err != nil {
+			slog.Error("service resolve failed", "service", prefix, "error", err)
+		}
 	}
 }
 
