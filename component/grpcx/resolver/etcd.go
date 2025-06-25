ce662c23e6b0dd3de7176ec638632ed3bb36939d
zed <zedsmail@163.com>
2025-06-25 13:00:03 +0800

    --

--- component/grpcx/resolver/etcd.go
+++ component/grpcx/resolver/etcd.go
@@ -121,65 +121,69 @@ func (r *etcdResolver) resolve(target string) (shouldWatch bool, addrs map[strin
 }
 
 func (r *etcdResolver) watch(prefix string, addrs map[string]resolver.Address, rev int64) {
-	slog.Debug("watch service", "prefix", prefix, "addrs", addrs)
-	ctx, cancel := context.WithCancel(context.Background())
-	opts := []clientv3.OpOption{
-		clientv3.WithPrefix(),
-	}
-	if rev != 0 {
-		opts = append(opts, clientv3.WithRev(rev+1))
-	}
-	rch := r.c.Watch(ctx, prefix, opts...)
-	for n := range rch {
-		if n.Err() != nil {
-			if errors.Is(n.Err(), rpctypes.ErrCompacted) {
-				slog.Debug("watch compacted", "prefix", prefix)
-			} else {
-				slog.Error("watch service failed", "prefix", prefix, "error", n.Err())
-			}
-			break
+	for {
+		slog.Debug("watch service", "prefix", prefix, "addrs", addrs)
+		ctx, cancel := context.WithCancel(context.Background())
+		opts := []clientv3.OpOption{
+			clientv3.WithPrefix(),
 		}
-
-		update := false
-		for _, ev := range n.Events {
-			switch ev.Type {
-			case mvccpb.PUT:
-				slog.Debug("receive put", "key", string(ev.Kv.Key), "value", string(ev.Kv.Value))
-				if _, ok := addrs[string(ev.Kv.Key)]; !ok {
-					addrs[string(ev.Kv.Key)] = resolver.Address{Addr: string(ev.Kv.Value)}
-					update = true
+		if rev != 0 {
+			opts = append(opts, clientv3.WithRev(rev+1))
+		}
+		rch := r.c.Watch(ctx, prefix, opts...)
+		for n := range rch {
+			if n.Err() != nil {
+				if errors.Is(n.Err(), rpctypes.ErrCompacted) {
+					slog.Debug("watch compacted", "prefix", prefix)
+				} else {
+					slog.Error("watch service failed", "prefix", prefix, "error", n.Err())
 				}
-			case mvccpb.DELETE:
-				slog.Debug("receive delete", "key", string(ev.Kv.Key), "value", string(ev.Kv.Value))
-				if _, ok := addrs[string(ev.Kv.Key)]; ok {
-					delete(addrs, string(ev.Kv.Key))
-					update = true
+				break
+			}
+
+			update := false
+			for _, ev := range n.Events {
+				switch ev.Type {
+				case mvccpb.PUT:
+					slog.Debug("receive put", "key", string(ev.Kv.Key), "value", string(ev.Kv.Value))
+					if _, ok := addrs[string(ev.Kv.Key)]; !ok {
+						addrs[string(ev.Kv.Key)] = resolver.Address{Addr: string(ev.Kv.Value)}
+						update = true
+					}
+				case mvccpb.DELETE:
+					slog.Debug("receive delete", "key", string(ev.Kv.Key), "value", string(ev.Kv.Value))
+					if _, ok := addrs[string(ev.Kv.Key)]; ok {
+						delete(addrs, string(ev.Kv.Key))
+						update = true
+					}
+				}
+			}
+
+			if update == true {
+				slog.Debug("update service state", "addrs", addrs)
+				err := r.cc.UpdateState(resolver.State{Addresses: maps.Values(addrs)})
+				if err != nil {
+					slog.Error("update service state failed", "service", prefix, "error", err)
 				}
 			}
 		}
+		cancel()
+		r.c.Close()
+		s := rand.IntN(10) + 1
+		time.Sleep(time.Second * time.Duration(s))
+		slog.Info("re new etcd client")
+		var err error
+		r.c, err = r.newClient()
+		if err != nil {
+			slog.Debug("re new etcd client failed", "error", err)
+			continue
+		}
 
-		if update == true {
-			slog.Debug("update service state", "addrs", addrs)
-			err := r.cc.UpdateState(resolver.State{Addresses: maps.Values(addrs)})
-			if err != nil {
-				slog.Error("update service state failed", "service", prefix, "error", err)
-			}
+		_, addrs, rev = r.resolve(prefix)
+		if err != nil {
+			slog.Error("service resolve failed", "service", prefix, "error", err)
 		}
 	}
-	cancel()
-	r.c.Close()
-	s := rand.IntN(10) + 1
-	time.Sleep(time.Second * time.Duration(s))
-	slog.Info("re new etcd client")
-	var err error
-	r.c, err = r.newClient()
-	if err != nil {
-		slog.Debug("re new etcd client failed", "error", err)
-		return
-	}
-
-	_, addrs, rev = r.resolve(prefix)
-	go r.watch(prefix, addrs, rev)
 }
 
 // EtcdResolverBuilder 需实现 Builder 接口
