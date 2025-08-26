18505b3e485ce4a588d71b809cbbeda2f7f92f78
zed <zedsmail@163.com>
2025-08-26 23:24:39 +0800

    feat: 添加NewSet方法.

--- core/tool/set.go
+++ core/tool/set.go
@@ -1,9 +1,20 @@
 package tool
 
 import (
-	"github.com/goslacker/slacker/core/mapx"
 	"sync"
+
+	"github.com/goslacker/slacker/core/mapx"
 )
+
+func NewSet[K comparable, S []K](s S) *Set[K] {
+	m := make(map[K]struct{})
+	for _, v := range s {
+		m[v] = struct{}{}
+	}
+	return &Set[K]{
+		inner: m,
+	}
+}
 
 type Set[K comparable] struct {
 	inner map[K]struct{}
