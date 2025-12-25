0447b72c3d764570a1e98b74ffbb212acd5a60c1
zed <zedsmail@163.com>
2025-12-25 18:14:16 +0800

    feat: 添加数据库事务持有器实现
    实现一个通用的GORM事务持有器，提供事务上下文管理功能。包含事务创建、提交、回滚等方法，支持通过上下文传递事务对象，简化事务管理流程。

---
+++ component/gormx/holder.go
@@ -0,0 +1,72 @@
+package gormx
+
+import (
+	"context"
+	"database/sql"
+
+	"gorm.io/gorm"
+)
+
+type txKey struct{}
+
+var TxKey = txKey{}
+
+type Holder interface {
+	WithContext(ctx context.Context) *DB
+	GetDB() *gorm.DB
+}
+
+func NewHolder(db *gorm.DB) Holder {
+	return &DB{DB: db}
+}
+
+type DB struct {
+	ctx context.Context
+	*gorm.DB
+}
+
+func (h *DB) GetDB() *gorm.DB {
+	return h.DB
+}
+
+func (h *DB) WithContext(ctx context.Context) *DB {
+	// 从上下文中获取事务
+	tx, ok := ctx.Value(TxKey).(*gorm.DB)
+	if ok {
+		return &DB{ctx: ctx, DB: tx.WithContext(ctx)}
+	}
+	return &DB{ctx: ctx, DB: h.DB.WithContext(ctx)}
+}
+
+func (h *DB) Transaction(f func(ctx context.Context) error, opts ...*sql.TxOptions) error {
+	return h.DB.Transaction(func(tx *gorm.DB) error {
+		ctx := context.WithValue(h.ctx, TxKey, tx)
+		return f(ctx)
+	}, opts...)
+}
+
+func (h *DB) Begin(opts ...*sql.TxOptions) (context.Context, error) {
+	tx := h.DB.Begin(opts...)
+	err := tx.Error
+	if err != nil {
+		return nil, err
+	}
+	ctx := context.WithValue(h.ctx, TxKey, tx)
+	return ctx, tx.Error
+}
+
+func (h *DB) BeginCtx(ctx context.Context, opts ...*sql.TxOptions) (context.Context, error) {
+	return h.WithContext(ctx).Begin(opts...)
+}
+
+func (h *DB) TransactionCtx(ctx context.Context, f func(ctx context.Context) error, opts ...*sql.TxOptions) error {
+	return h.WithContext(ctx).Transaction(f, opts...)
+}
+
+func (h *DB) Commit(ctx context.Context) error {
+	return h.WithContext(ctx).DB.Commit().Error
+}
+
+func (h *DB) Rollback(ctx context.Context) error {
+	return h.WithContext(ctx).DB.Rollback().Error
+}
