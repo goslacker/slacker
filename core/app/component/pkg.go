3904071a29af7b1e7039162316a42fa66fabceb6
zed <zedsmail@163.com>
2025-12-25 17:40:17 +0800

    add

--- core/app/component/pkg.go
+++ core/app/component/pkg.go
@@ -1,5 +1,9 @@
 package component
 
+import (
+	"github.com/goslacker/slacker/core/app"
+)
+
 func Init(init func() error) *simpleComponent {
 	return &simpleComponent{
 		init: init,
@@ -16,5 +20,16 @@ func SimpleComponent(init, boot func() error) *simpleComponent {
 	return &simpleComponent{
 		init: init,
 		boot: boot,
+	}
+}
+
+func AfterInit(afterInit func()) *simpleComponent {
+	return &simpleComponent{
+		init: func() error {
+			app.RegisterListener(func(event app.AfterInit) {
+				afterInit()
+			})
+			return nil
+		},
 	}
 }
