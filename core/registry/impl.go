7cb1f4e42562a1defa9f5ef47f99804b46f47fde
zed <zedsmail@163.com>
2026-02-24 16:55:08 +0800

    --

--- core/registry/impl.go
+++ core/registry/impl.go
@@ -54,7 +54,6 @@ func NewEtcdDriver(c *clientv3.Client) *EtcdDriver {
 }
 
 func (e *EtcdDriver) Register(ctx context.Context, service string, addr string) (err error) {
-	println(1111111)
 	var resp *clientv3.LeaseGrantResponse
 	{
 		resp, err = e.c.Grant(ctx, 20)
@@ -62,7 +61,6 @@ func (e *EtcdDriver) Register(ctx context.Context, service string, addr string) 
 			return fmt.Errorf("grant lease failed: %w", err)
 		}
 	}
-	println(222222)
 
 	key := service + "/" + strconv.FormatInt(int64(resp.ID), 10)
 
@@ -72,7 +70,6 @@ func (e *EtcdDriver) Register(ctx context.Context, service string, addr string) 
 		return
 	}
 	slog.Info("register service success", "service", service, "addr", addr)
-	println(333333)
 	var (
 		ch     <-chan *clientv3.LeaseKeepAliveResponse
 		cancel context.CancelFunc
@@ -89,7 +86,6 @@ func (e *EtcdDriver) Register(ctx context.Context, service string, addr string) 
 			return fmt.Errorf("keep service '%s' alive failed: %w", service, err)
 		}
 	}
-	println(444444)
 
 	go func() {
 		for range ch {
@@ -106,7 +102,6 @@ func (e *EtcdDriver) Register(ctx context.Context, service string, addr string) 
 			}
 		}
 	}()
-	println(5555555)
 
 	return
 }
