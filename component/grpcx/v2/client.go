e6e100fe6b36f28511f73dc73a4f1362a2cae80f
zed <zedsmail@163.com>
2026-02-24 16:22:55 +0800

    feat(grpcx): 添加客户端keepalive参数配置
    为grpc客户端添加keepalive参数配置，设置连接保活时间为10秒，超时时间为5秒，以优化长连接场景下的连接稳定性

--- component/grpcx/v2/client.go
+++ component/grpcx/v2/client.go
@@ -3,6 +3,7 @@ package grpcx
 import (
 	"errors"
 	"math"
+	"time"
 
 	"github.com/goslacker/slacker/core/app"
 	"github.com/goslacker/slacker/core/container"
@@ -11,6 +12,7 @@ import (
 	"github.com/goslacker/slacker/core/registry"
 	"github.com/goslacker/slacker/core/trace"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 )
 
 func NewClient[T any](target string, provider func(cc grpc.ClientConnInterface) T, opts ...grpc.DialOption) (result T, err error) {
@@ -26,5 +28,9 @@ func NewClient[T any](target string, provider func(cc grpc.ClientConnInterface) 
 	opts = append(opts, grpc.WithUnaryInterceptor(trace.UnaryTraceClientInterceptor), grpc.WithStreamInterceptor(trace.StreamTraceClientInterceptor))
 	opts = append(opts, grpc.WithUnaryInterceptor(interceptor.UnaryThroughClientInterceptor), grpc.WithStreamInterceptor(interceptor.StreamThroughClientInterceptor))
 	opts = append(opts, grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(math.MaxInt32), grpc.MaxCallSendMsgSize(math.MaxInt32)))
+	opts = append(opts, grpc.WithKeepaliveParams(keepalive.ClientParameters{
+		Time:    10 * time.Second,
+		Timeout: 5 * time.Second,
+	}))
 	return grpcx.NewClient(target, provider, opts...)
 }
